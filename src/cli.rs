aeef3368a2052177257e8499c9b57efd05326d3f
Matheus L. P. <contact@matheuslp.com>
2024-01-26 10:12:52 -0600

    refactor: re-structure commands and cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,92 +1,16 @@
 use clap::Parser;
 
-use crate::{commands::Commands, user_list::UserList, username::Username};
+use crate::{commands::Command, user_list::UserList};
 
 #[derive(Debug, Parser)]
 #[command(about = "A CLI to manage users", long_about = None)]
 pub struct Cli {
     #[command(subcommand)]
-    command: Commands,
+    command: Command,
 }
 
 impl Cli {
     pub fn parse_command(&self, users: &mut UserList) {
-        // Save for later
-        // match self.command {
-        //     Commands::Add {
-        //         username,
-        //         name,
-        //         money,
-        //     } => todo!(),
-        //     Commands::Insert {
-        //         username,
-        //         name,
-        //         money,
-        //     } => todo!(),
-        //     Commands::Edit {
-        //         username,
-        //         name,
-        //         money,
-        //     } => todo!(),
-        //     Commands::Withdraw { username, amount } => todo!(),
-        //     Commands::Deposit { username, amount } => todo!(),
-        //     Commands::Transfer { from, to, amount } => todo!(),
-        //     Commands::Delete { username } => todo!(),
-        // }
-    }
-}
-
-fn insert(users: &mut UserList, username: &Username, name: &str, money: f64) {
-    todo!()
-}
-fn edit(users: &mut UserList, username: &Username, name: Option<&str>, money: Option<f64>) {
-    todo!()
-}
-fn withdraw(users: &mut UserList, username: &Username, amount: f64) {
-    todo!()
-}
-fn deposit(users: &mut UserList, username: &Username, amount: f64) {
-    todo!()
-}
-fn transfer(users: &mut UserList, from: &Username, to: &Username, amount: f64) {
-    todo!()
-}
-fn delete(users: &mut UserList, username: &Username) {
-    todo!()
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_parse_command() {
-        todo!()
-    }
-    #[test]
-    fn test_add() {
-        todo!()
-    }
-    #[test]
-    fn test_insert() {
-        todo!()
-    }
-    #[test]
-    fn test_edit() {
-        todo!()
-    }
-    #[test]
-    fn test_withdraw() {
-        todo!()
-    }
-    #[test]
-    fn test_deposit() {
-        todo!()
-    }
-    #[test]
-    fn test_transfer() {
-        todo!()
-    }
-    #[test]
-    fn test_delete() {
         todo!()
     }
 }
