050ed540ad06d7079bb298b5accc2065309802b5
Matheus L. P. <contact@matheuslp.com>
2024-02-04 00:49:02 -0600

    feat: implement UserList and User fully

--- src/user.rs
+++ src/user.rs
@@ -15,7 +15,11 @@ pub struct User {
 
 impl Display for User {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        if self.name.is_empty() {
+            write!(f, "Unnamed User | {}", self.money)
+        } else {
+            write!(f, "{} | {}", self.name, self.money)
+        }
     }
 }
 
